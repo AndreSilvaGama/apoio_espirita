cea09463b51f19c157ff48de94041fdad74eaaa7
Vigia diário <actions@github.com>
2026-09-13 14:44:15 +0000

    chore: atualiza a estrutura do banco (automático)

--- apoio_espirita-main/supabase/schema.sql
+++ apoio_espirita-main/supabase/schema.sql
@@ -6,7 +6,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict MZmyScvQH1yRS0FfTs9jLC3XAh7k8S7SNAkDF1sLZ1ouf3jaH4fNTMUie16srgx
+\restrict 8xdV0Zbjx0c769guYNvqyM9YZfHoLgPx0nNT89wf6Aq9735ytYaLouZRX8LY8Hs
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
@@ -7368,5 +7368,5 @@ CREATE POLICY votos_select ON public.painel_votes FOR SELECT USING ((auth.role()
 -- PostgreSQL database dump complete
 --
 
-\unrestrict MZmyScvQH1yRS0FfTs9jLC3XAh7k8S7SNAkDF1sLZ1ouf3jaH4fNTMUie16srgx
+\unrestrict 8xdV0Zbjx0c769guYNvqyM9YZfHoLgPx0nNT89wf6Aq9735ytYaLouZRX8LY8Hs
 
